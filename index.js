d69c021b447e2175fe787123cd9e02b0e9e2eaa3
mahabub ahmed <mahabub.ahmed.eee@gmail.com>
2022-05-08 14:25:01 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.v6zrn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const itemCollection = client.db("mrAppler").collection("item");
+
+    app.get("/item", async (req, res) => {
+      const query = {};
+      const cursor = itemCollection.find(query);
+      const items = await cursor.toArray();
+      res.send(items);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Running Mr. Appler Server");
+});
+
+//post data
+app.post("/item", async (req, res) => {
+  const itemCollection = client.db("mrAppler").collection("item");
+  const newItem = req.body;
+  const result = await itemCollection.insertOne(newItem);
+  res.send(result);
+});
+
+//delete
+app.delete("/item/:id", async (req, res) => {
+  const itemCollection = client.db("mrAppler").collection("item");
+  const id = req.params.id;
+  const query = { _id: ObjectId(id) };
+  const result = await itemCollection.deleteOne(query);
+  res.send(result);
+});
+
+app.listen(port, () => {
+  console.log("Listening to port", port);
+});
